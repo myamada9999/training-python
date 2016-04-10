cc24a111259e6f8bea01e33f8d1e9479a052f82f
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-10 20:59:05 +0900

    test-*.py: Add test scripts.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ test-string.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+# copied from http://www.python-izm.com/contents/introduction/syntax.shtml
+
+print 'python-izm'
+print "python-izm"
+
+test_str = """
+test_1
+test_2
+"""
+print test_str
+
+test_str2 = "python"
+test_str2 = test_str2 + "-"
+test_str2 = test_str2 + "izm"
+print test_str2
+
+
---
+++ test-syntax.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# copied from http://www.python-izm.com/contents/introduction/syntax.shtml
+
+print u"load module"
+
+def test():
+	print u"function: call test"
+
+if __name__ == "__main__":
+
+	print "python-izm"
+	# print "python-izm"
+	test()
