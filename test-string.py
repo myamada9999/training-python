eace0243054898e7172fa34006392452d0653872
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-12 20:29:40 +0900

    test-string.py: Update script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

--- test-string.py
+++ test-string.py
@@ -31,3 +31,33 @@ print test_str4.replace("izm", "ism")
 
 test_str5 = "python-izm"
 print test_str5.split("-")
+
+test_str6 = "1234"
+print test_str6.rjust(10, "0")
+print test_str6.rjust(10, "!")
+print test_str6.zfill(10)
+print test_str6.zfill(3)
+
+test_str7 = "python-izm"
+print test_str7.startswith("python")
+print test_str7.startswith("izm")
+print "z" in test_str7
+print "s" in test_str7
+
+test_str8 = "PyThon-Izm"
+print test_str8.upper()
+print test_str8.lower()
+
+test_str8 = "    python-izm.com"
+print test_str8
+test_str8 = test_str8.lstrip()
+print test_str8
+test_str8 = test_str8.lstrip("python")
+print test_str8
+
+test_str9 = "python-izm.com    "
+print test_str9 + "/"
+test_str9 = test_str9.rstrip()
+print test_str9 + "/"
+test_str9 = test_str9.rstrip("com")
+print test_str9
