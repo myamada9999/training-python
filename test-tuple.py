9bb3929d985abd9b6984808626789abdca9ead5f
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-14 21:53:49 +0900

    test-tuple.py: Add new script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ test-tuple.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import datetime
+
+def getToday():
+	today = datetime.datetime.today()
+	value = (today.year, today.month, today.day)
+
+	return value
+
+if __name__ == "__main__":
+	test_tuple = getToday()
+	print test_tuple
+	print test_tuple[0]
+	print test_tuple[1]
+	print test_tuple[2]
