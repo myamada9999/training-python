829c54cd1f7112c2ead0443bf6a127db8e30f45e
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-18 22:19:52 +0900

    test-dict.py: Add some processes for dictonary.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

--- test-dict.py
+++ test-dict.py
@@ -19,3 +19,26 @@ print test_dict_1.get('YEARS')
 print '------------------------------------------------------------'
 print test_dict_1.get('YEAR', 'NOT FOUND')
 print test_dict_1.get('YEARS', 'NOT FOUND')
+
+
+print '------------------------------------------------------------'
+
+test_dict_2 = {}
+test_dict_2['YEAR'] = '2010'
+test_dict_2['MONTH'] = '1'
+test_dict_2['DAY'] = '20'
+print test_dict_2
+
+print '------------------------------------------------------------'
+del test_dict_2['DAY']
+print test_dict_2
+
+print '------------------------------------------------------------'
+print test_dict_2.keys()
+
+print '------------------------------------------------------------'
+print test_dict_2.has_key('YEAR')
+print test_dict_2.has_key('DAY')
+
+print 'YEAR' in test_dict_2
+print 'DAY' in test_dict_2
