524053cabdbe33fd2b0a72988c737cabcb4074c1
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-19 22:02:31 +0900

    test-comment.py: Add new script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ test-comment.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+if __name__ == "__main__":
+	# comment out
+#	print "test"
+	print "test" # comment out
+
+	# comment out
+	'''
+	print "test"
+	print "test"
+	'''
+
+	# nest
+	'''
+	print "test"
+	"""
+	print "test"
+	"""
+	'''
+
