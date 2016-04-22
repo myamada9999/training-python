0768b3c36f54f0d6056998d775324df9a552b11f
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-22 21:08:59 +0900

    test-arg.py: Add new script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ test-arg.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+import sys
+
+if __name__ == "__main__":
+
+	param = sys.argv
+	print param
+
+	print "1st args: " + param[1]
+	print "2nd args: " + param[2]
+	print "3rd args: " + param[3]
+
