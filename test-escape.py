2fce8453f4c39c01c7fae65d05ba985e71678767
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-24 21:37:58 +0900

    test-escape.py: Add new script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ test-escape.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+if __name__ == "__main__":
+	print '1234\'56789'
+	print "1234'56789"
+
+	print "1234\"56789"
+	print '1234"56789'
+
+	print "1234\\56789"
+
+	print "1234\n56789"
+
+	print "1234\t56789"
