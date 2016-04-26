93b9f0ce30644b9d998972d71f4dc01ea4e13481
Masahiro Yamada <masahiro9999@gmail.com>
2016-04-26 20:01:02 +0900

    test-loop.py: Add new script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ test-loop.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+if __name__ == "__main__":
+
+	for_sample = []
+	for_sample.append("python")
+	for_sample.append("-")
+	for_sample.append("izm")
+	for_sample.append("for")
+	for_sample.append("statement")
+	for_sample.append("sample")
+
+	for value in for_sample:
+		print value
+
