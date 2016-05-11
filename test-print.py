469d3206fe0bc2bdea4b88715dca70a15390d6f6
Masahiro Yamada <masahiro9999@gmail.com>
2016-05-11 21:07:49 +0900

    test-print.py: Add new script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ test-print.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+print "python"
+print "-"
+print "izm"
+print "com"
+
+print "python" ,
+print "-" ,
+print "izm" ,
+print "com"
+
+print "%s" % "python-izm.com"
+print "%s-%s.%s" % ("python","izm","com")
+test_int = 100 + 100
+test_str = "python-izm.com"
+print "%d days, %s" % (test_int, test_str)
